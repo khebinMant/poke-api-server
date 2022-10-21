54277763d947fffd531c90ed9ec34f7c7122ae70
khabLexander <mantillagka@hotmail.com>
2022-10-20 23:16:36 -0500

    make it better

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -35,7 +35,7 @@ module.exports = {
         dialect: 'postgres',
         dialectOptions: {
             ssl: true
-          }
+        }
     },
     test: {
         username: 'root',
@@ -50,13 +50,11 @@ module.exports = {
         }
     },
     production: {
-        url:'postgres://wpnihjifucjriv:d6add6a2db54ab4cd676e906a51180542f33a3ffab06f807ff9d354f26bc7796@ec2-54-82-205-3.compute-1.amazonaws.com:5432/del5rhv3fkjii9',
+        url:'postgres://zlhhhkuakbmmkz:720b04512f2400d11c8ebb685b45a1a9c7850ee8afc3e784bc38b2d140f62d85@ec2-52-4-87-74.compute-1.amazonaws.com:5432/d7t1ce7lcad7dq',
         dialect: 'postgres',
         operatorsAliases: 0,
-        dialectOptions:{
-            ssl:{
-                rejectUnauthorized:false
-            }
+        dialectOptions: {
+            ssl: true
         }
     }
 }
