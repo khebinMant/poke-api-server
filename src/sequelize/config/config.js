70f48718accc8656b4bd129fbfd8e8021961c802
khabLexander <mantillagka@hotmail.com>
2022-10-21 01:02:48 -0500

    make it better

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -32,15 +32,15 @@ module.exports = {
         // database: process.env.NAME_DB || 'pokemon_db',
         // host: process.env.HOST_DB || 'localhost',
         // dialect: 'postgres',
-        url:'postgres://zlhhhkuakbmmkz:720b04512f2400d11c8ebb685b45a1a9c7850ee8afc3e784bc38b2d140f62d85@ec2-52-4-87-74.compute-1.amazonaws.com:5432/d7t1ce7lcad7dq',
-        dialect: 'postgres',
-        operatorsAliases: 0,
-        dialectOptions: {
-            ssl: {
-              require: true,
-              rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-            }
-          },
+        // url:'postgres://zlhhhkuakbmmkz:720b04512f2400d11c8ebb685b45a1a9c7850ee8afc3e784bc38b2d140f62d85@ec2-52-4-87-74.compute-1.amazonaws.com:5432/d7t1ce7lcad7dq',
+        // dialect: 'postgres',
+        // operatorsAliases: 0,
+        // dialectOptions: {
+        //     ssl: {
+        //       require: true,
+        //       rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+        //     }
+        //   },
     },
     test: {
         username: 'root',
